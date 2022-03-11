dfc3d05597ec219c791b8e2c38c5c54c6b5323b0
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2022-03-10 23:14:29 -0300

    definindo id teclas projeto

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const display = document.getElementById('display');
+const numeros = document.querySelectorAll('[id*=tecla]');
+
+const inserirNumero = (evento) = display.textContent = evento.target.textContent;
+
+numeros.forEach(numero = numero.addEventListener('click', inserirNumero));
